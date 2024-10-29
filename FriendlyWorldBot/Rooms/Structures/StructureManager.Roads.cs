2ffad060fd736b97faa3b6f4141c54189b0b4b8c
Steffi Schulz <stefanie.schulz@it-titans.de>
2024-10-29 14:30:46 +0100

    don|t place road on mountains

--- FriendlyWorldBot/Rooms/Structures/StructureManager.Roads.cs
+++ FriendlyWorldBot/Rooms/Structures/StructureManager.Roads.cs
@@ -37,18 +37,24 @@ public partial class StructureManager {
         var minY = maxSpawnPositions.Select(p => p.Y).Min();
         var maxX = maxSpawnPositions.Select(p => p.X).Max();
         var maxY = maxSpawnPositions.Select(p => p.Y).Max();
-
+        
         foreach (var spawn in _room.SpawnsForExtensionConstruction) {
             var spawnPosition = spawn.LocalPosition;
+            var terrains = _room.Room.LookForAtArea<IStructureWall>(new Position(minX + spawnPosition.X, minY + spawnPosition.Y), new Position(maxX + spawnPosition.X, maxY + spawnPosition.Y)).ToList();
 
             for (var x = minX; x <= maxX; x++) {
                 for (var y = minY; y <= maxY; y++) {
                     if (IsValidRoadPosition(x, y)) {
-                        var constructionResult = _room.Room.CreateConstructionSite<IStructureRoad>(new Position(x + spawnPosition.X, y + spawnPosition.Y));
-                        if (constructionResult == RoomCreateConstructionSiteResult.Ok) {
-                            roadCount++;
-                            if (roadCount >= MaxConstructionSites) {
-                                return true;
+                        var roomX = x + spawnPosition.X;
+                        var roomY = y + spawnPosition.Y;
+                        if (!terrains.Any(t => t.LocalPosition.X == roomX && t.LocalPosition.Y == roomY))
+                        {
+                            var constructionResult = _room.Room.CreateConstructionSite<IStructureRoad>(new Position(roomX, roomY));
+                            if (constructionResult == RoomCreateConstructionSiteResult.Ok) {
+                                roadCount++;
+                                if (roadCount >= MaxConstructionSites) {
+                                    return true;
+                                }
                             }
                         }
                     }
